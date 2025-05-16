73b9001451fce160f4e455802b5aa1882a04dc42
Harshit Dubey <131461213+harshit02237@users.noreply.github.com>
2025-05-16 08:23:52 +0530

    Update AppD.jsx

--- src/Components/AppD.jsx
+++ src/Components/AppD.jsx
@@ -6,7 +6,7 @@ function AppD({ todoName, todoDate, onDeleteClick }) {
                 <div className="col-4">{todoDate}</div>
                 <div className="col-2">
                     <button type="button" className="btn btn-danger kg-button" onClick={() => {onDeleteClick(todoName)}}>
-                        Delete
+                        Del
                     </button>
                 </div>
             </div>
